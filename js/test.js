9b927b12dfdd3b2cafa4feaa605c27d8af74fbb2
Lars <lars.walderhaug@hotmail.com>
2021-08-19 22:45:23 +0200

    tester container js api

---
+++ js/test.js
@@ -0,0 +1,17 @@
+const container = document.querySelector(".container");
+
+async function getProducts(url) {
+  const response = await fetch(url);
+  const products = await response.json();
+  console.log(products);
+
+  products.forEach(function (product) {
+    container.innerHTML += `<div class="product">
+        <h2>${product.name}</h2>
+        <div class="prod-img"><img src="${product.images[0].src}"><div>
+        <p>${product.price_html}</p>
+      </div>`;
+  });
+}
+
+getProducts("https://rainydays.thefed.no/wp-json/wc/store/products");
