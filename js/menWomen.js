4911e6a6e96f16cc0dcef6a295a7391961e1b11b
Lars <lars.walderhaug@hotmail.com>
2021-08-21 19:40:32 +0200

    trying to fix the hyperlinks for men

--- js/menWomen.js
+++ js/menWomen.js
@@ -80,3 +80,38 @@ async function getProducts(url) {
   });
 }
 getProducts("https://rainydays.thefed.no/wp-json/wc/store/products");
+
+const newProductsLink = document.querySelector(".new_products .newProductsLink");
+const test = document.querySelector(".product_overview");
+
+console.log(newProductsLink);
+newProductsLink.addEventListener("click", function () {
+  console.log(hello);
+  async function getProducts(url) {
+    const response = await fetch(url);
+    console.log(response);
+    const products = await response.json();
+    console.log(products);
+    products.forEach(function (product) {
+      for (let i = 0; i < product.categories.length; i++) {
+        if (product.categories[i].slug === "new-men") {
+          test.innerHTML = `
+                <div class="product_overview_content product_overview_contentTest">
+                  <div class="woman_imgTest">
+                  <a href="product-specific.html?id=${product.id}" class="women_specific">
+                    <img src="${product.images[0].src}">
+                  </a>
+                  </div>
+                  <div>
+                  <a href="product-specific.html?id=${product.id}" class="women_specific">
+                     <h3>${product.name}</h3>
+                    </a>
+                     <p>${product.prices.price} kr</p>
+                  </div>
+                </div>`;
+        }
+      }
+    });
+  }
+  getProducts("https://rainydays.thefed.no/wp-json/wc/store/products");
+});
